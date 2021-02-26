f62b35330de9f62dac35e833fe70e5109eb2e7ba
Gustavo Martini <38445129+martinigustavo@users.noreply.github.com>
2021-02-26 16:27:40 -0300

    .

--- gobarber-backend/src/modules/users/services/ResetPasswordService.spec.ts
+++ gobarber-backend/src/modules/users/services/ResetPasswordService.spec.ts
@@ -7,16 +7,16 @@ import ResetPasswordService from './ResetPasswordService';
 
 let fakeUsersRepository: FakeUsersRepository;
 let fakeUserTokensRepository: FakeUserTokensRepository;
-let resetPasswordService: ResetPasswordService;
 let fakeHashProvider: FakeHashProvider;
+let resetPassword: ResetPasswordService;
 
-describe('SendForgotPasswordEmail', () => {
+describe('ResetPassword', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepository();
     fakeUserTokensRepository = new FakeUserTokensRepository();
     fakeHashProvider = new FakeHashProvider();
 
-    resetPasswordService = new ResetPasswordService(
+    resetPassword = new ResetPasswordService(
       fakeUsersRepository,
       fakeUserTokensRepository,
       fakeHashProvider,
@@ -34,7 +34,7 @@ describe('SendForgotPasswordEmail', () => {
 
     const generateHash = jest.spyOn(fakeHashProvider, 'generateHash');
 
-    await resetPasswordService.execute({
+    await resetPassword.execute({
       password: '123456',
       token,
     });
@@ -43,5 +43,50 @@ describe('SendForgotPasswordEmail', () => {
 
     expect(generateHash).toHaveBeenCalledWith('123456');
     expect(updatedUser?.password).toBe('123456');
+  });
+
+  it('should not be able to reset the password with non-existing token', async () => {
+    await expect(
+      resetPassword.execute({
+        token: 'non-existing-token',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to reset the password with non-existing user', async () => {
+    const { token } = await fakeUserTokensRepository.generate(
+      'non-existing-user',
+    );
+
+    await expect(
+      resetPassword.execute({
+        token,
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to reset the password after 2 hours', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Zé Ninguem',
+      email: 'zeninguem@exemplo.com',
+      password: '123123',
+    });
+
+    const { token } = await fakeUserTokensRepository.generate(user.id);
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      const customDate = new Date();
+
+      return customDate.setHours(customDate.getHours() + 3);
+    });
+
+    await expect(
+      resetPassword.execute({
+        password: '123456',
+        token,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
--- gobarber-backend/src/modules/users/services/ResetPasswordService.ts
+++ gobarber-backend/src/modules/users/services/ResetPasswordService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { isAfter, addHours } from 'date-fns';
 
 // import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -36,6 +37,13 @@ class ResetPasswordService {
 
     if (!user) {
       throw new AppError('User does not exist');
+    }
+
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired');
     }
 
     user.password = await this.hashProvider.generateHash(password);
