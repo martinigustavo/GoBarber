d452c53863888262ee9540a05508601e55747df6
Gustavo Martini <38445129+martinigustavo@users.noreply.github.com>
2021-03-09 14:44:43 -0300

    implemented etherealmailprovider

--- gobarber-backend/src/shared/infra/http/server.ts
+++ gobarber-backend/src/shared/infra/http/server.ts
@@ -16,7 +16,7 @@ const app = express();
 const useCors = cors();
 app.use(useCors);
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
