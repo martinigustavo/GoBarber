b0d536235329bba159ba2614a82137754d77e56f
Gustavo Martini <38445129+martinigustavo@users.noreply.github.com>
2021-03-24 16:25:27 -0300

    chore: finished implementing ListProvidersService

--- gobarber-backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ gobarber-backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -2,6 +2,7 @@ import { v4 } from 'uuid';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 import User from '../../infra/typeorm/entities/User';
 
@@ -20,7 +21,9 @@ class FakeUsersRepository implements IUsersRepository {
     return findUser;
   }
 
-  public async findAllProviders(except_user_id?: string): Promise<User[]> {
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
     let { users } = this;
 
     if (except_user_id) {
